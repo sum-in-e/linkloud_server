e870d70ea053ee3afa4cf738af0dfd13ed887500
sumin <suminkim.me@gmail.com>
2023-05-04 12:54:48 +0900

    fix: setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-unused-vars': 'error', // 사용하지 않은 변수에 에러 표시
+    '@typescript-eslint/no-unused-vars': 'warn', // 사용하지 않은 변수에 에러 표시
     'no-console': 'warn', // 콘솔 사용에 경고 표시
   },
 };
