7a6f8f03b4e3a5d8c34d51611569a0bc2412f547
sumin <suminkim.me@gmail.com>
2023-05-04 19:58:47 +0900

    Add: 스웨거 설정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from 'src/core/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService); // ConfigService 주입
+  const configService = app.get(ConfigService);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Linkloud')
+    .setDescription('Linkloud API description')
+    .setVersion('1.0')
+    .addTag('Linkloud')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  const swaggerID = configService.get('SWAGGER_USER');
+  const swaggerPW = configService.get('SWAGGER_PASSWORD');
+
+  // Add basic auth middleware
+  app.use(
+    ['/docs', '/docs-json'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [swaggerID]: swaggerPW,
+      },
+    }),
+  );
+
+  SwaggerModule.setup('docs', app, document);
+
   const port = configService.get('PORT');
   await app.listen(port);
 }
