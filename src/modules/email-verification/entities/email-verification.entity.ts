2a05a202b4fddf0de5d657d54a96d0b9db278a33
sumin <suminkim.me@gmail.com>
2023-06-13 23:37:43 +0900

    Fix: 이메일 인증 시 제일 마지막 이메일 코드와 비교하도록 수정

--- src/modules/email-verification/entities/email-verification.entity.ts
+++ src/modules/email-verification/entities/email-verification.entity.ts
@@ -12,18 +12,18 @@ export class EmailVerification extends BaseEntity {
   email!: string;
 
   @Column({ type: 'varchar', length: 6 })
-  verification_code!: string;
+  verificationCode!: string;
 
   @Column({ type: 'datetime' })
-  expired_at!: Date;
+  expiredAt!: Date;
 
   @Column({ type: 'boolean', default: false }) // 이메일 인증 여부 컬럼
-  is_verified!: boolean;
+  isVerified!: boolean;
 
   @BeforeInsert() // 데이터를 저장하기 전에 실행할 메서드
   setExpiredAt() {
     const date = new Date(); // 현재 시간을 나타내는 Date 객체 생성
     date.setMinutes(date.getMinutes() + 10); // 데이터 저장되는 시간으로부터 10분 후 인증번호 만료 설정
-    this.expired_at = date; // expired_at에 할당
+    this.expiredAt = date; // expiredAt에 할당
   }
 }
