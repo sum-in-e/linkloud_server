bae1173bcf3e60e53939e6adf688fcd8055ee462
sumin <suminkim.me@gmail.com>
2023-07-17 20:00:20 +0900

    Fix: cron-job schedule

--- src/modules/notification/scheduler/notificationScheduler.ts
+++ src/modules/notification/scheduler/notificationScheduler.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import { NotificationService } from 'src/modules/notification/services/notification.service';
 
 @Injectable()
 export class NotificationScheduler {
   constructor(private readonly notificationService: NotificationService) {}
-  @Cron(CronExpression.EVERY_MINUTE)
-  @Cron('0 0 21 * * *', { timeZone: 'Asia/Seoul' }) // 매일 오후 9시 (KST)
+  @Cron('0 21 * * 2', { timeZone: 'Asia/Seoul' }) // 매주 화요일 오후 9시 (KST)
+  @Cron('0 21 * * 4', { timeZone: 'Asia/Seoul' }) // 매주 목요일 오후 9시 (KST)
+  @Cron('0 21 * * 0', { timeZone: 'Asia/Seoul' }) // 매주 일요일 오후 9시 (KST)
   async handleSundayCron(): Promise<void> {
     await this.notificationService.sendNotification();
   }
 }
-
-// @Cron('0 21 * * 2', { timeZone: 'Asia/Seoul' }) // 매주 화요일 오후 9시 (KST)
-// @Cron('0 22 * * 4', { timeZone: 'Asia/Seoul' }) // 매주 목요일 오후 10시 (KST)
-// @Cron('0 22 * * 0', { timeZone: 'Asia/Seoul' }) // 매주 일요일 오후 10시 (KST)
