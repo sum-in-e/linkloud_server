b1c1608b55f7c68781446f2bbd3ee47ffd233b22
sumin <suminkim.me@gmail.com>
2023-07-21 16:49:06 +0900

    Fix: dev DB 연동&prod DB 초기화&prod DB와 dev DB sync 맞추기

--- src/configs/datasource.ts
+++ src/configs/datasource.ts
@@ -25,14 +25,14 @@ const connectionSource = new DataSource({
   username: dbUsername,
   password: dbPassword,
   database: dbName,
-  timezone: '+09:00', //  TypeORM이 KST(UTC+9:00) 시간을 사용하도록 강제
-  poolSize: 20, // TypeORM이 사용할 데이터베이스 연결 수의 제한을 설정. 20이면 TypeORM은 동시에 20개의 데이터베이스 연결을 유지함
-  entities: ['dist/src/modules/**/entities/*.entity.{ts,js}'], // 엔티티의 경로를 __dirname으로 못읽기 때문에 dist를 이용
-  synchronize: JSON.parse(dbSync), // true면 entity class 변경될때마다 테이블 자동 변경됨. 테이블이 DB에 없는 상태여도 entity를 import하면 테이블이 자동으로 생긴다. entity가 수정되어 DB에 반영되어도 할당된 값들이 날아가지는 않지만 production에서는 false로 해야 데이터 손실을 막을 수 있다.
+  timezone: '+00:00',
+  poolSize: 20,
+  entities: ['dist/src/modules/**/entities/*.entity.{ts,js}'],
+  synchronize: JSON.parse(dbSync),
   logging: JSON.parse(dbLogging),
-  namingStrategy: new SnakeNamingStrategy(), // TypeORM의 엔티티의 필드명을 자동으로 스네이크 케이스로 변환해주는 네이밍 전략 클래스(ex. deletedAt -> daleted_at으로 DB 컬럼 저장됨)
+  namingStrategy: new SnakeNamingStrategy(),
   extra: {
-    decimalNumbers: true, // TypeORM이 숫자 값을 반환할 때 소수점 이하 자릿수를 유지할 것인지 여부를 설정하는 옵션 (true: 소숫점 이하 자릿수 유지)
+    decimalNumbers: true,
   },
   // * 마이그레이션 설정
   migrations: ['dist/src/migrations/*.{ts,js}'], // 마이그레이션할 파일 경로
--- src/migrations/1689924417800-file.ts
+++ src/migrations/1689924417800-file.ts
@@ -1,26 +1,29 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class file1687420256983 implements MigrationInterface {
-  name = 'file1687420256983';
+export class file1689924417800 implements MigrationInterface {
+  name = 'file1689924417800';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE TABLE \`email_verification\` (\`id\` int UNSIGNED NOT NULL AUTO_INCREMENT, \`email\` varchar(255) NOT NULL, \`verification_code\` varchar(6) NOT NULL, \`expired_at\` datetime NOT NULL, \`is_verified\` tinyint NOT NULL DEFAULT 0, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
+      `CREATE TABLE \`subscription\` (\`id\` int NOT NULL AUTO_INCREMENT, \`endpoint\` text NOT NULL, \`keys\` json NOT NULL, \`user_id\` int UNSIGNED NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
       `CREATE TABLE \`user\` (\`id\` int UNSIGNED NOT NULL AUTO_INCREMENT, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NULL, \`name\` varchar(255) NOT NULL, \`method\` varchar(255) NOT NULL, \`is_inactive\` tinyint NOT NULL DEFAULT 0, \`inactived_at\` datetime NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` datetime(6) NULL, \`last_login_at\` datetime NULL, UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE \`link\` (\`id\` int UNSIGNED NOT NULL AUTO_INCREMENT, \`url\` varchar(255) NOT NULL, \`thumbnail_url\` varchar(255) NOT NULL, \`title\` text NOT NULL, \`description\` text NOT NULL, \`memo\` text NOT NULL, \`is_in_my_collection\` tinyint NOT NULL DEFAULT 0, \`is_read\` tinyint NOT NULL DEFAULT 0, \`read_at\` datetime NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` datetime(6) NULL, \`kloud_id\` int UNSIGNED NULL, \`user_id\` int UNSIGNED NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
       `CREATE TABLE \`kloud\` (\`id\` int UNSIGNED NOT NULL AUTO_INCREMENT, \`name\` varchar(50) NOT NULL, \`position\` int NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` datetime(6) NULL, \`user_id\` int UNSIGNED NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `CREATE TABLE \`link\` (\`id\` int UNSIGNED NOT NULL AUTO_INCREMENT, \`url\` varchar(255) NOT NULL, \`thumbnail_url\` varchar(255) NOT NULL, \`title\` text NOT NULL, \`description\` text NOT NULL, \`memo\` text NOT NULL, \`test\` text NOT NULL, \`is_in_my_collection\` tinyint NOT NULL DEFAULT 0, \`is_read\` tinyint NOT NULL DEFAULT 0, \`read_at\` datetime NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` datetime(6) NULL, \`kloud_id\` int UNSIGNED NULL, \`user_id\` int UNSIGNED NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
-    );
-    await queryRunner.query(
       `CREATE TABLE \`kakao_verication_info\` (\`id\` int UNSIGNED NOT NULL AUTO_INCREMENT, \`sub\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `ALTER TABLE \`kloud\` ADD CONSTRAINT \`FK_eee5bfddc34bc9f8e31314a4f2f\` FOREIGN KEY (\`user_id\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`,
+      `ALTER TABLE \`subscription\` ADD CONSTRAINT \`FK_940d49a105d50bbd616be540013\` FOREIGN KEY (\`user_id\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`,
     );
     await queryRunner.query(
       `ALTER TABLE \`link\` ADD CONSTRAINT \`FK_cc70feed445e3443c5cfa0fc1ac\` FOREIGN KEY (\`kloud_id\`) REFERENCES \`kloud\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`,
@@ -28,17 +31,22 @@ export class file1687420256983 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE \`link\` ADD CONSTRAINT \`FK_da35233ec2bfaa121bb3540039b\` FOREIGN KEY (\`user_id\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`,
     );
+    await queryRunner.query(
+      `ALTER TABLE \`kloud\` ADD CONSTRAINT \`FK_eee5bfddc34bc9f8e31314a4f2f\` FOREIGN KEY (\`user_id\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`kloud\` DROP FOREIGN KEY \`FK_eee5bfddc34bc9f8e31314a4f2f\``);
     await queryRunner.query(`ALTER TABLE \`link\` DROP FOREIGN KEY \`FK_da35233ec2bfaa121bb3540039b\``);
     await queryRunner.query(`ALTER TABLE \`link\` DROP FOREIGN KEY \`FK_cc70feed445e3443c5cfa0fc1ac\``);
-    await queryRunner.query(`ALTER TABLE \`kloud\` DROP FOREIGN KEY \`FK_eee5bfddc34bc9f8e31314a4f2f\``);
+    await queryRunner.query(`ALTER TABLE \`subscription\` DROP FOREIGN KEY \`FK_940d49a105d50bbd616be540013\``);
     await queryRunner.query(`DROP TABLE \`kakao_verication_info\``);
-    await queryRunner.query(`DROP TABLE \`link\``);
     await queryRunner.query(`DROP TABLE \`kloud\``);
+    await queryRunner.query(`DROP TABLE \`link\``);
     await queryRunner.query(`DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\``);
     await queryRunner.query(`DROP TABLE \`user\``);
+    await queryRunner.query(`DROP TABLE \`subscription\``);
     await queryRunner.query(`DROP TABLE \`email_verification\``);
   }
 }
