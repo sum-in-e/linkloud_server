f0408d126c330e924822772857ec25fb6644c8fb
sumin <suminkim.me@gmail.com>
2023-08-16 20:13:38 +0900

    Fix: 가이드 링크 수정

--- src/modules/link/constants/guide-links.constant.ts
+++ src/modules/link/constants/guide-links.constant.ts
@@ -1,38 +1,13 @@
-const url = 'https://linkloud.co.kr';
 const thumbnailUrl =
   'https://res.cloudinary.com/dqcgvbbv7/image/upload/v1688032357/linkloud/linkloud_thumbnail_cp3joj.png';
 
 export const guideLinks = [
   {
-    url,
+    url: 'https://www.craft.me/s/AGjkOZUm2mFTDE',
     thumbnailUrl,
-    title: '[가이드] 링크 상세 확인하기',
-    description: '저장한 링크를 누르면 상세 정보를 확인하고 수정할 수 있어요!',
-    memo: '링크에 대한 생각이나 내용을 요약하여 기록해보세요! 기록은 정보를 흡수하는 데 도움을 주고 내용을 오래 기억할 수 있도록 해줍니다.🥸',
-    kloud: null,
-  },
-  {
-    url,
-    thumbnailUrl,
-    title: '[가이드] 저장한 웹사이트 방문하기',
-    description: `“새 탭으로 보기” 버튼을 누르면 저장한 웹사이트를 새 탭으로 볼 수 있어요!`,
-    memo: '',
-    kloud: null,
-  },
-  {
-    url,
-    thumbnailUrl,
-    title: '[가이드] 여러개의 링크 선택하기',
-    description: `“선택하기” 버튼을 누르면 여러개의 링크를 손쉽게 분류하고 정리할 수 있어요!`,
-    memo: '링크에 대한 생각이나 내용을 요약하여 기록해보세요! 기록은 정보를 흡수하는 데 도움을 주고 내용을 오래 기억할 수 있도록 해줍니다.🥸',
-    kloud: null,
-  },
-  {
-    url,
-    thumbnailUrl,
-    title: '[가이드] 내 컬렉션으로 링크 소장하기',
+    title: '[클릭] 사용 가이드 문서를 읽어주세요!',
     description:
-      '특별하게 보관하고 싶은 링크가 있나요? 링크를 내 컬렉션으로 지정해 보세요! 저장한 링크의 상세 정보에서 설정할 수 있습니다.',
+      '링클라우드를 이용하는 데 도움이 되는 정보가 있어요! 읽어보면 링클라우드와 빠르게 친해질 수 있을 거예요👀',
     memo: '',
     kloud: null,
   },
